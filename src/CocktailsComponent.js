05588593f71fbbc096c4418fcd66effc5bf535b9
gabrielaivascu <gabriela.ivascu95@gmail.com>
2021-06-03 09:29:00 +0300

    add form

--- src/CocktailsComponent.js
+++ src/CocktailsComponent.js
@@ -4,9 +4,17 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 const CocktailsComponent = (props) => {
-
+  
   const [cocktailsState, setCocktailsState] = useState({ cocktails: [], isLoaded: false, isBack: true });
 
+  const [formState, setForm] = useState({cocktailName: ''});
+
+  const  handleChangeData = e => {
+    setForm({...formState, [e.target.name]: e.target.value });
+  }
+  const submitForm = () => {
+    console.log(formState);
+  }
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
@@ -26,6 +34,14 @@ const CocktailsComponent = (props) => {
   
     return(
       <div className="cocktails-page">
+        <div className="form-container">
+          <form>
+              <label>Cocktail name</label>
+              <input className="input-container" type="text" name="cocktailName" value={formState.cocktailName} onChange={handleChangeData}></input>
+          </form>
+          <button onClick={submitForm}>Submit</button>
+        </div>
+    
         <div className="container-column">
           <h2>Non_Alcoholic cocktails</h2>
           <div className="category-blocks-list">
